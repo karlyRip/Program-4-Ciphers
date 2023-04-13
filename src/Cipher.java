bfdb793706bec2eeb7550ac4299820f47887440c
RIPPERKJ22 <RipperKJ22@gcc.edu>
2023-04-13 09:33:38 -0400

    I defined the method bodies for the encryption and decryption of strings.

--- src/Cipher.java
+++ src/Cipher.java
@@ -4,10 +4,14 @@ public abstract class Cipher {
     abstract public void decrypt (char c);
 
     public void encrypt (String s) {
-
+        for(int i = 0; i < s.length(); i++){
+            encrypt(s.charAt(i));
+        }
     }
     public void decrypt (String s) {
-
+        for(int i = s.length()-1; i >= 0; i--){
+            encrypt(s.charAt(i));
+        }
     }
     abstract public Cipher newCopy();
 }
