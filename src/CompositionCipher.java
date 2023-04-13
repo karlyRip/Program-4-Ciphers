c369b39f969f4faa4d3d5ee346c9b44157f97c94
winona-w <wherleyws21@gcc.edu>
2023-04-13 10:21:17 -0400

    I created the CompositionCipher class and added most of the methods.  The copy constructor still needs to be finished but everything else seems decent

---
+++ src/CompositionCipher.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+public class CompositionCipher extends Cipher{
+    private ArrayList<Cipher> ciphers;
+
+    public CompositionCipher() {
+        ciphers = new ArrayList<>();
+    }
+
+    public CompositionCipher(CompositionCipher other) {
+        //TODO
+    }
+
+    @Override
+    public char encrypt (char c) {
+        for (int i = 0; i < ciphers.size(); i++) {
+            c = ciphers.get(i).encrypt(c);
+        }
+        return c;
+    }
+
+    @Override
+    public char decrypt (char c) {
+        for (int i = ciphers.size() - 1; i >= 0; i--) {
+            c = ciphers.get(i).decrypt(c);
+        }
+        return c;
+    }
+
+    public void add (Cipher theCipher) {
+        ciphers.add(theCipher);
+    }
+
+    @Override
+    public CompositionCipher newCopy() {
+        return new CompositionCipher(this);
+    }
+}
